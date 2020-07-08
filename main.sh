3752c6c54d05fb4170fe5b23f6cc6676ec1429b0
Ajiit <cloud_user@ajiit1c.mylabserver.com>
2020-07-08 17:19:41 +0000

    main.sh

---
+++ main.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/globalekt/aws.git
+cd aws/
+vi CF_CreateENI.json
+git status
+git add CF_CreateENI.json
+git commit -m "CF_CreateENI.json"
+git push -u origin master
+username: ajiit@globalekt.com
+passwd: Ind1!
